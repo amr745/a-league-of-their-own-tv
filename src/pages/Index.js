a9a98f4438a250d796f7a614cf1b22fd92b45ae1
Ashley Richard <ashleymrichard@gmail.com>
2023-02-02 23:13:17 -0800

    old paper properly fits cards

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -4,26 +4,25 @@ function Index(props) {
   // loaded function
   const loaded = () => {
     return props.players.map((player) => (
-      <div className="old-paper">
-       <img src="https://i.imgur.com/4rKVgAQ.png" alt="overlay" />
-      <div className="overlay">
-       <div key={player._id} className="card">
-         {/* <div className="old-paper"></div> */}
+      <div className="card-container">
+        <div className="old-paper-overlay"></div>
+      <div className="card-content">
+      <div key={player._id} className="card">
         <Link to={`/players/${player._id}`}>
-        <img className="team_logo" src={player.team} alt="team logo"/>
-        <img className="player" src={player.image} alt={player.name} />
-        <figcaption className="name">{player.name}<br /> {player.position}</figcaption>
-        </Link>
-       </div>
-       </div>
+              <img className="team_logo" src={player.team} alt="team logo"/>
+              <img className="player" src={player.image} alt={player.name} />
+              <figcaption className="name">{player.name}<br /> {player.position}</figcaption>
+            </Link>
+          </div>
       </div>
+     </div>
     ))
   }
-
+  
   const loading = () => {
     return <h1>Loading...</h1>
   }
-
+  
   return props.players ? loaded() : loading()
 }
 
